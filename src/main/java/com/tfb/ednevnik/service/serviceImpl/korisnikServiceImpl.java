b3e39e0795dc67e61f27316455dba3d1656c9ad1
nurfo47 <nurfet.mehulic00@gmail.com>
2024-05-25 12:43:15 +0200

    Created method for save user

---
+++ src/main/java/com/tfb/ednevnik/service/serviceImpl/korisnikServiceImpl.java
@@ -0,0 +1,61 @@
+package com.tfb.ednevnik.service.serviceImpl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.tfb.ednevnik.admindto.korisnikDto;
+import com.tfb.ednevnik.model.korisnik;
+import com.tfb.ednevnik.service.korisnikService;
+import com.tfb.ednevnik.repository.korisnikRepository;
+@Service
+public class korisnikServiceImpl implements korisnikService{
+    
+    @Autowired
+    private korisnikRepository korisnikRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void saveKorisnik(korisnikDto korisnikDto) {
+        korisnik korisnik = new korisnik();
+        korisnik.setIme(korisnikDto.getIme());
+        korisnik.setPrezime(korisnikDto.getPrezime());
+        korisnik.setEmail(korisnikDto.getEmail());
+        korisnik.setUsername(korisnikDto.getUsername());
+        korisnik.setLozinka(passwordEncoder.encode(korisnikDto.getLozinka()));
+        korisnik.setMobitel(korisnikDto.getMobitel());
+        korisnik.setDatum(korisnikDto.getDatum());
+        korisnik.setJmbg(korisnikDto.getJmbg());
+        korisnik.setTip(korisnikDto.getTip());
+        korisnikRepository.save(korisnik);
+
+}
+
+    @Override
+    public List<korisnik> getAllKorisnik() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllKorisnik'");
+    }
+
+    @Override
+    public korisnik findKorisnikById(long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findKorisnikById'");
+    }
+
+    @Override
+    public void deleteKorisnikById(long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteKorisnikById'");
+    }
+
+    @Override
+    public korisnik updateKorisnik(korisnik korisnik) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateKorisnik'");
+    }
+}
