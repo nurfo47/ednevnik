f3b80e4c8e45ba9e127e2a7ec9b33af9fa2cf67a
nurfo47 <nurfet.mehulic00@gmail.com>
2024-05-25 12:42:33 +0200

    Created CRUD  methods for implementation

--- src/main/java/com/tfb/ednevnik/service/korisnikService.java
+++ src/main/java/com/tfb/ednevnik/service/korisnikService.java
@@ -2,13 +2,12 @@ package com.tfb.ednevnik.service;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
+import com.tfb.ednevnik.admindto.korisnikDto;
 import com.tfb.ednevnik.model.korisnik;
-@Service
+
 public interface korisnikService {
     List<korisnik> getAllKorisnik();
-    korisnik saveKorisnik(korisnik korisnik);
+    void saveKorisnik(korisnikDto korisnikDto);
     korisnik findKorisnikById(long id);
     void deleteKorisnikById(long id);
     korisnik updateKorisnik(korisnik korisnik);
