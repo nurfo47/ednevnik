1d7625b30aea5b2929208405bae091c0ca21cd6c
nurfo47 <nurfet.mehulic00@gmail.com>
2024-05-25 14:39:24 +0200

    Added declaration of method to find user by jmbg

--- src/main/java/com/tfb/ednevnik/repository/korisnikRepository.java
+++ src/main/java/com/tfb/ednevnik/repository/korisnikRepository.java
@@ -3,12 +3,12 @@ package com.tfb.ednevnik.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.tfb.ednevnik.model.korisnik;
+import com.tfb.ednevnik.model.Korisnik;
 
 @Repository
-public interface korisnikRepository extends JpaRepository<korisnik, Long>{
-    korisnik findById(long id);
-    korisnik findByUsername(String username);
-    korisnik findByJmbg(String jmbg);
-    korisnik findByTip(String tip);
+public interface korisnikRepository extends JpaRepository<Korisnik, Long>{
+    Korisnik findById(long id);
+    Korisnik findByUsername(String username);
+    Korisnik findByJmbg(String jmbg);
+    Korisnik findByTip(String tip);
 }
