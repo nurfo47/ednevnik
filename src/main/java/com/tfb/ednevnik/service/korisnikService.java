0f075fc5fda10a7237d0cb738d1434048c43c876
nurfo47 <nurfet.mehulic00@gmail.com>
2024-05-25 14:41:31 +0200

    Changed type for method saveKorisnik

--- src/main/java/com/tfb/ednevnik/service/korisnikService.java
+++ src/main/java/com/tfb/ednevnik/service/korisnikService.java
@@ -3,12 +3,12 @@ package com.tfb.ednevnik.service;
 import java.util.List;
 
 import com.tfb.ednevnik.admindto.korisnikDto;
-import com.tfb.ednevnik.model.korisnik;
+import com.tfb.ednevnik.model.Korisnik;
 
 public interface korisnikService {
-    List<korisnik> getAllKorisnik();
+    List<Korisnik> getAllKorisnik();
     void saveKorisnik(korisnikDto korisnikDto);
-    korisnik findKorisnikById(long id);
+    Korisnik findKorisnikById(long id);
     void deleteKorisnikById(long id);
-    korisnik updateKorisnik(korisnik korisnik);
+    Korisnik updateKorisnik(Korisnik korisnik);
 }
