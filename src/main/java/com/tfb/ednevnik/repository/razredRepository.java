ce59a7851f39ce7c30f818b18d8347fcf45e38c2
nurfo47 <nurfet.mehulic00@gmail.com>
2024-05-25 14:40:06 +0200

    Added declaration of method to find razred by Id

--- src/main/java/com/tfb/ednevnik/repository/razredRepository.java
+++ src/main/java/com/tfb/ednevnik/repository/razredRepository.java
@@ -3,10 +3,10 @@ package com.tfb.ednevnik.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.tfb.ednevnik.model.razred;
+import com.tfb.ednevnik.model.Razred;
 
 
 @Repository
-public interface razredRepository extends JpaRepository<razred, Long>{
-    razred findById(long id);
+public interface razredRepository extends JpaRepository<Razred, Long>{
+    Razred findById(long id);
 }
