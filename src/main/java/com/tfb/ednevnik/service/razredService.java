c73bdd05946a1cd52e4f914366fd6f01620dd2fc
nurfo47 <nurfet.mehulic00@gmail.com>
2024-06-01 19:57:19 +0200

    Defined service methods

--- src/main/java/com/tfb/ednevnik/service/razredService.java
+++ src/main/java/com/tfb/ednevnik/service/razredService.java
@@ -1,14 +1,12 @@
 package com.tfb.ednevnik.service;
 
 import java.util.List;
-
-import org.springframework.stereotype.Service;
+import java.util.Optional;
 
 import com.tfb.ednevnik.model.Razred;
-@Service
 public interface razredService {
     List<Razred> getAllRazred();
-    Razred getRazredById(Long id);
+    Optional<Razred> getRazredById(Long id);
     Razred saveRazred(Razred razred);
     void deleteRazredById(Long id);
 }
