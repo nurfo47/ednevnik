ede83d8dbdbe760f6f1cec1075d24706758871bf
nurfo47 <nurfet.mehulic00@gmail.com>
2024-06-02 00:10:24 +0200

    Added service method to find by id

--- src/main/java/com/tfb/ednevnik/service/adminService.java
+++ src/main/java/com/tfb/ednevnik/service/adminService.java
@@ -1,7 +1,9 @@
 package com.tfb.ednevnik.service;
 
 import com.tfb.ednevnik.admindto.adminDto;
+import com.tfb.ednevnik.model.admin;
 
 public interface adminService {
     void save(adminDto adminDto);
+    admin findById(long id);
 }
