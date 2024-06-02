18af26f56e62f9f9d1eaaf206fb8b90705d47ab8
nurfo47 <nurfet.mehulic00@gmail.com>
2024-06-02 12:15:01 +0200

    Fixed case error

--- src/main/java/com/tfb/ednevnik/repository/adminRepository.java
+++ src/main/java/com/tfb/ednevnik/repository/adminRepository.java
@@ -3,14 +3,11 @@ package com.tfb.ednevnik.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.tfb.ednevnik.model.admin;
-
-
-
+import com.tfb.ednevnik.model.Admin;
 
 @Repository
-public interface adminRepository extends JpaRepository<admin, Long> {
-    admin findByEmail(String email);
-    admin findByUsername(String username);
-    admin findById(long id);
+public interface adminRepository extends JpaRepository<Admin, Long> {
+    Admin findByEmail(String email);
+    Admin findByUsername(String username);
+    Admin findById(long id);
 }
