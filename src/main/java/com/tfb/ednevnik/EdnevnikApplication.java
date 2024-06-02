64239f21396b736157642f808631a22ddc105587
nurfo47 <nurfet.mehulic00@gmail.com>
2024-06-02 12:17:32 +0200

    Added entity scan annotation

--- src/main/java/com/tfb/ednevnik/EdnevnikApplication.java
+++ src/main/java/com/tfb/ednevnik/EdnevnikApplication.java
@@ -2,11 +2,13 @@ package com.tfb.ednevnik;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude= {SecurityAutoConfiguration.class})
 @ComponentScan(basePackages = {"com.tfb.ednevnik"})
+@EntityScan(basePackages = {"com.tfb.ednevnik.model"})
 public class EdnevnikApplication {
 
 	public static void main(String[] args) {
