523ca8a2988335a732bae2f70bbbe246c3b3b439
nurfo47 <nurfet.mehulic00@gmail.com>
2024-06-02 15:14:17 +0200

    Created function to open page on click for razredi

--- src/main/resources/static/javascript/script.js
+++ src/main/resources/static/javascript/script.js
@@ -1,3 +1,7 @@
 function redirectToProfile(korisnikId) {
     window.location.href = '/korisnik-profil/' + korisnikId;
 }
+
+function redirectToUceniciList(razredId) {
+    window.location.href = '/razred/' + razredId + '/ucenici';
+}
