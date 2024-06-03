49d7b32dc05a73ffd22206f2133892170fff1cbd
nurfo47 <nurfet.mehulic00@gmail.com>
2024-06-03 17:30:45 +0200

    Defined method to find subjects

--- src/main/java/com/tfb/ednevnik/repository/korisnikRepository.java
+++ src/main/java/com/tfb/ednevnik/repository/korisnikRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.tfb.ednevnik.model.Korisnik;
+import com.tfb.ednevnik.model.Predmet;
 
 
 @Repository
@@ -18,5 +19,6 @@ public interface korisnikRepository extends JpaRepository<Korisnik, Long>{
     List<Korisnik> findByRazredId(Long id);
     @SuppressWarnings("null")
     List<Korisnik> findAllById(@SuppressWarnings("null") Iterable<Long> ids);
+    List<Korisnik> findByPredmeti(Predmet predmet);
     
 }
