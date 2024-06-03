845fe15d15413407e89ef814f632c40edc6c4caf
nurfo47 <nurfet.mehulic00@gmail.com>
2024-06-03 16:42:44 +0200

    Defined method to find subjects by id

--- src/main/java/com/tfb/ednevnik/repository/predmetRepository.java
+++ src/main/java/com/tfb/ednevnik/repository/predmetRepository.java
@@ -1,11 +1,13 @@
 package com.tfb.ednevnik.repository;
 
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.tfb.ednevnik.model.Predmet;
 @Repository
 public interface predmetRepository extends JpaRepository<Predmet, Long>{
-    
+    Set<Predmet> findAllById(Long id);
 }
