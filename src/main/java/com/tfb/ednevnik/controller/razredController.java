cde59c7674dc0c5eae1c0cfca96ef32b435dca61
nurfo47 <nurfet.mehulic00@gmail.com>
2024-06-03 16:39:18 +0200

    Implemented methods to add Students in different Class

--- src/main/java/com/tfb/ednevnik/controller/razredController.java
+++ src/main/java/com/tfb/ednevnik/controller/razredController.java
@@ -1,14 +1,20 @@
 package com.tfb.ednevnik.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
+import com.tfb.ednevnik.model.Korisnik;
 import com.tfb.ednevnik.model.Razred;
+import com.tfb.ednevnik.service.korisnikService;
 import com.tfb.ednevnik.service.razredService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -18,6 +24,8 @@ public class razredController {
     
     @Autowired
     private razredService razredService;
+    @Autowired
+    private korisnikService korisnikService;
 
     @GetMapping("/add-razred")
     public String addRazredPage() {
@@ -38,6 +46,20 @@ public class razredController {
         return "/razredi";
     }
     
-    
+     @GetMapping("/razredi/addKorisnici/{id}")
+    public String showAddKorisniciToRazred(@PathVariable Long id, Model model) {
+        Razred razred = razredService.findById(id);
+        List<Korisnik> korisnici = korisnikService.findByTip("UCENIK");
+        model.addAttribute("razredId", id); // Pass only the ID
+        model.addAttribute("razred", razred); // Pass the entire Razred object if needed
+        model.addAttribute("korisnici", korisnici);
+        return "dodaj-u-razred";
+    }
+
+    @PostMapping("/razredi/addKorisnici")
+    public String addKorisniciToRazred(@RequestParam Long razredId, @RequestParam List<Long> korisnikIds) {
+        razredService.addKorisniciToRazred(razredId, korisnikIds);
+        return "redirect:/razredi";
+    }
     
 }
