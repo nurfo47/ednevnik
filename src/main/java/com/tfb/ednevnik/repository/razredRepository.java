85ef44d7dbe59fefb187b552d64321e7b455fada
nurfo47 <nurfet.mehulic00@gmail.com>
2024-06-04 17:08:12 +0200

    No changes

--- src/main/java/com/tfb/ednevnik/repository/razredRepository.java
+++ src/main/java/com/tfb/ednevnik/repository/razredRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import com.tfb.ednevnik.model.Razred;
 import com.tfb.ednevnik.model.Korisnik;
+
 import java.util.List;
 
 
@@ -16,4 +17,5 @@ public interface razredRepository extends JpaRepository<Razred, Long>{
         Optional<Razred> findById(long id);
         Razred getById(long id);
         List<Razred> findByProfesori(Korisnik profesori);
+        
 }
