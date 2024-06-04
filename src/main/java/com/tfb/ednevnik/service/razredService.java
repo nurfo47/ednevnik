483198d0a6de925ddc7752b2e72d59e7b8ab3378
nurfo47 <nurfet.mehulic00@gmail.com>
2024-06-04 14:23:53 +0200

    Defined service methods to list razredi by user

--- src/main/java/com/tfb/ednevnik/service/razredService.java
+++ src/main/java/com/tfb/ednevnik/service/razredService.java
@@ -3,6 +3,7 @@ package com.tfb.ednevnik.service;
 import java.util.List;
 import java.util.Optional;
 
+import com.tfb.ednevnik.model.Korisnik;
 import com.tfb.ednevnik.model.Razred;
 public interface razredService {
     List<Razred> getAllRazred();
@@ -11,4 +12,5 @@ public interface razredService {
     void deleteRazredById(Long id);
     void addKorisniciToRazred(Long razredId, List<Long> korisnikId);
     Razred findById(Long id);
+    List<Razred> findByKorisnik(Korisnik korisnik);
 }
