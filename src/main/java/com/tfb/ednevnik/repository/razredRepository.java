840d99a67f38c516afcbbc1387e9607087bb682f
nurfo47 <nurfet.mehulic00@gmail.com>
2024-06-04 14:22:32 +0200

    Added definiton of method to find razred by teacher

--- src/main/java/com/tfb/ednevnik/repository/razredRepository.java
+++ src/main/java/com/tfb/ednevnik/repository/razredRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.tfb.ednevnik.model.Razred;
+import com.tfb.ednevnik.model.Korisnik;
+import java.util.List;
+
 
 
 @Repository
 public interface razredRepository extends JpaRepository<Razred, Long>{
         Optional<Razred> findById(long id);
         Razred getById(long id);
+        List<Razred> findByProfesori(Korisnik profesori);
 }
