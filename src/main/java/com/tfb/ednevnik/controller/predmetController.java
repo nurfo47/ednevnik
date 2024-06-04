9c4f0d202643bed3e67d888f96811f6289ae1eaa
nurfo47 <nurfet.mehulic00@gmail.com>
2024-06-04 14:21:15 +0200

    Implemented controller method to list assigned subjects at teacher profile

--- src/main/java/com/tfb/ednevnik/controller/predmetController.java
+++ src/main/java/com/tfb/ednevnik/controller/predmetController.java
@@ -1,13 +1,18 @@
 package com.tfb.ednevnik.controller;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.tfb.ednevnik.model.Korisnik;
 import com.tfb.ednevnik.model.Predmet;
+import com.tfb.ednevnik.service.korisnikService;
 import com.tfb.ednevnik.service.predmetService;
 
 
@@ -16,6 +21,8 @@ public class predmetController {
     
     @Autowired
     private predmetService predmetService;
+    @Autowired
+    private korisnikService korisnikService;
 
     @GetMapping("/add-predmet")
     public String addPredmetPage(){
@@ -33,5 +40,15 @@ public class predmetController {
     public String getAllPredmeti(Model model){
         model.addAttribute("predmet", predmetService.getAllPredmet());
         return "predmeti";
+    }
+
+    @GetMapping("/profesor-dashboard/predmeti")
+    public String listPredmetiForNastavnik(Model model, Authentication authentication) {
+        String loggedUsername = authentication.getName();
+        Korisnik korisnik = korisnikService.findKorisnikByUsername(loggedUsername);
+        Set<Predmet> predmeti = korisnik.getPredmeti();
+        model.addAttribute("predmeti", predmeti);
+        model.addAttribute("korisnik", korisnik);
+        return "predmeti-for-korisnik";
     }
 }
