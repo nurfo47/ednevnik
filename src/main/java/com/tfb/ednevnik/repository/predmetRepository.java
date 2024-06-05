2f6743d60fd891aa256230678b93aa8ff55f7bf8
nurfo47 <nurfet.mehulic00@gmail.com>
2024-06-05 11:27:26 +0200

    Updated repository to find users and classes by subjects

--- src/main/java/com/tfb/ednevnik/repository/predmetRepository.java
+++ src/main/java/com/tfb/ednevnik/repository/predmetRepository.java
@@ -1,5 +1,6 @@
 package com.tfb.ednevnik.repository;
 
+import java.util.List;
 import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,6 +10,9 @@ import com.tfb.ednevnik.model.Predmet;
 @Repository
 public interface predmetRepository extends JpaRepository<Predmet, Long>{
     Set<Predmet> findAllById(Long id);
+    @SuppressWarnings("null")
+    List<Predmet> findKorisnikById(Long korisnikId);
+    List<Predmet> findByRazredi_Id(Long razredId);
 
     
 }
