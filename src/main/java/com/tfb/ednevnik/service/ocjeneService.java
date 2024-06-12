6a8afa6562fffce39ac51ba4083f95c16f21d084
nurfo47 <nurfet.mehulic00@gmail.com>
2024-06-12 22:39:07 +0200

    Created service interface and added methods

---
+++ src/main/java/com/tfb/ednevnik/service/ocjeneService.java
@@ -0,0 +1,16 @@
+package com.tfb.ednevnik.service;
+
+import java.util.List;
+import java.util.Set;
+
+import com.tfb.ednevnik.model.Korisnik;
+import com.tfb.ednevnik.model.Ocjene;
+import com.tfb.ednevnik.model.Predmet;
+
+public interface ocjeneService {
+    Set<Ocjene> getAllOcjene(Long id);
+    void saveOcjena(Ocjene ocjena);
+    List<Ocjene> getOcjeneByKorisnik(Long korisnikId);
+    List<Ocjene> findOcjeneByKorisnikAndPredmet(Korisnik korisnik, Predmet predmet);
+    
+}
