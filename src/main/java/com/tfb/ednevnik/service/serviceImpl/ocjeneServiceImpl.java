f953ab6c53b994fa7356abfe72ca5940c4b4bac2
nurfo47 <nurfet.mehulic00@gmail.com>
2024-06-12 22:39:27 +0200

    Implemented service methods

---
+++ src/main/java/com/tfb/ednevnik/service/serviceImpl/ocjeneServiceImpl.java
@@ -0,0 +1,40 @@
+package com.tfb.ednevnik.service.serviceImpl;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tfb.ednevnik.model.Korisnik;
+import com.tfb.ednevnik.model.Ocjene;
+import com.tfb.ednevnik.model.Predmet;
+import com.tfb.ednevnik.repository.ocjeneRepository;
+import com.tfb.ednevnik.service.ocjeneService;
+
+@Service
+public class ocjeneServiceImpl implements ocjeneService {
+
+    @Autowired
+    private ocjeneRepository ocjeneRepository;
+    @Override
+    public Set<Ocjene> getAllOcjene(Long id) {
+        return ocjeneRepository.getAllById(id);
+    }
+
+    @Override
+    public void saveOcjena(Ocjene ocjena) {
+        ocjeneRepository.save(ocjena);
+    }
+
+    @Override
+    public List<Ocjene> getOcjeneByKorisnik(Long korisnikId) {
+       return ocjeneRepository.findAllByKorisnikId(korisnikId);
+    }
+
+    @Override
+    public List<Ocjene> findOcjeneByKorisnikAndPredmet(Korisnik korisnik, Predmet predmet) {
+        return ocjeneRepository.findByKorisnikAndPredmet(korisnik, predmet);
+    }
+    
+}
