9eb6f62a88b7faa28daa4209fa15756a5ec00ae5
nurfo47 <nurfet.mehulic00@gmail.com>
2024-06-15 19:37:23 +0200

    Created repository for izostanci

---
+++ src/main/java/com/tfb/ednevnik/repository/izostanciRepository.java
@@ -0,0 +1,17 @@
+package com.tfb.ednevnik.repository;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tfb.ednevnik.model.Izostanci;
+import com.tfb.ednevnik.model.Korisnik;
+import com.tfb.ednevnik.model.Razred;
+@Repository
+public interface izostanciRepository extends JpaRepository<Izostanci, Long>{
+    Set<Izostanci> getAllById(Long id);
+    List<Izostanci> findAllByKorisnikId(Long id);
+    List<Izostanci> findByKorisnikAndRazred(Korisnik korisnik, Razred razred);
+}
