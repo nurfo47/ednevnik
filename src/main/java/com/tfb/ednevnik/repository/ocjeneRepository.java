7dd8fb99b2a8d30a3b281ca7d0bd84f3563e2457
nurfo47 <nurfet.mehulic00@gmail.com>
2024-06-21 19:29:51 +0200

    Created query repositroy to retrive avg grade for each subject for selected student

--- src/main/java/com/tfb/ednevnik/repository/ocjeneRepository.java
+++ src/main/java/com/tfb/ednevnik/repository/ocjeneRepository.java
@@ -5,6 +5,8 @@ import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.tfb.ednevnik.model.Korisnik;
 import com.tfb.ednevnik.model.Ocjene;
@@ -15,4 +17,7 @@ public interface ocjeneRepository extends JpaRepository<Ocjene, Long>{
     Set<Ocjene> getAllById(Long id);
     List<Ocjene> findAllByKorisnikId(Long id);
     List<Ocjene> findByKorisnikAndPredmet(Korisnik korisnik, Predmet predmet);
+
+     @Query("SELECT AVG(o.ocjena) FROM Ocjene o WHERE o.predmet.id = :predmetId AND o.korisnik.id = :korisnikId")
+    Double findAverageOcjenaByPredmetIdAndKorisnikId(@Param("predmetId") Long predmetId, @Param("korisnikId") Long korisnikId);
 }
