08177bcd33fa8b2a3f041b9cde26538342f735cb
nurfo47 <nurfet.mehulic00@gmail.com>
2024-06-21 18:49:53 +0200

    Created query methods to get count of izostanci

--- src/main/java/com/tfb/ednevnik/repository/izostanciRepository.java
+++ src/main/java/com/tfb/ednevnik/repository/izostanciRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.tfb.ednevnik.model.Izostanci;
@@ -15,4 +17,16 @@ public interface izostanciRepository extends JpaRepository<Izostanci, Long>{
     List<Izostanci> findAllByKorisnikId(Long id);
     List<Izostanci> findByKorisnikAndRazred(Korisnik korisnik, Razred razred);
     List<Izostanci> findByKorisnik(Korisnik korisnik);
+
+    //Brojac svih izostanaka korisnika
+    @Query("SELECT COUNT(i) FROM Izostanci i WHERE i.korisnik.id = :korisnikId AND i.korisnik.tip = 'UCENIK'")
+    long countIzostanciByKorisnikIdAndTipUcenik(@Param("korisnikId") Long korisnikId);
+
+    //Brojac opravdanih izostanaka
+    @Query("SELECT COUNT(i) FROM Izostanci i WHERE i.korisnik.id = :korisnikId AND i.opravdanost = 'OPRAVDANO'")
+    long countOpravdaniIzostanciByKorisnikId(Long korisnikId);
+
+    //Brojac neopravdanih izostanaka
+    @Query("SELECT COUNT(i) FROM Izostanci i WHERE i.korisnik.id = :korisnikId AND i.opravdanost = 'NEOPRAVDANO'")
+    long countNeopravdaniIzostanciByKorisnikId(Long korisnikId);
 }
