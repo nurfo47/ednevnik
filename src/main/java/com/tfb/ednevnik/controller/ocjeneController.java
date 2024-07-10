6c713de9ff4a2d4cd463a1c787638017c71950f7
nurfo47 <nurfet.mehulic00@gmail.com>
2024-07-10 21:16:56 +0200

    Deleted methods for ocjene views and added method to edit ocjena

--- src/main/java/com/tfb/ednevnik/controller/ocjeneController.java
+++ src/main/java/com/tfb/ednevnik/controller/ocjeneController.java
@@ -8,7 +8,6 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,11 +16,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.tfb.ednevnik.service.ocjeneService;
 import com.tfb.ednevnik.service.predmetService;
-import com.tfb.ednevnik.service.razredService;
 import com.tfb.ednevnik.model.Korisnik;
 import com.tfb.ednevnik.model.Ocjene;
 import com.tfb.ednevnik.model.Predmet;
-import com.tfb.ednevnik.model.Razred;
+
 import com.tfb.ednevnik.service.korisnikService;
 
 @Controller
@@ -35,9 +33,6 @@ public class ocjeneController {
 
     @Autowired
     private predmetService predmetService;
-
-    @Autowired
-    private razredService razredService;
 
     //Prikaz forme za unos ocjene iz određenog predmeta
     @GetMapping("/razredi/{razredId}/ucenici/{korisnikId}/ocjene/add")
@@ -79,101 +74,21 @@ public class ocjeneController {
         return "redirect:/razredi/" + razredId + "/ucenici";
     }
 
-    //Prikaz svih ocjena iz predmeta za jednog ucenika od strane admina
-    @GetMapping("/admin-dashboard/{korisnikId}/predmeti/{predmetId}/ocjene")
-    public String listOcjeneForPredmet(@PathVariable Long korisnikId, @PathVariable Long predmetId, Model model) {
-        // Dohvati korisnika i predmet
-        Korisnik korisnik = korisnikService.findKorisnikById(korisnikId);
-        Predmet predmet = predmetService.getPredmetById(predmetId);
-        
-        // Dohvati sve ocjene za korisnika i predmet
-        List<Ocjene> ocjene = ocjeneService.findOcjeneByKorisnikAndPredmet(korisnik, predmet);
-        Double averageOcjena = ocjeneService.findAverageOcjenaByPredmetIdAndKorisnikId(predmetId, korisnikId);
-        
-        // Pass the grades to the view
-        model.addAttribute("korisnik", korisnik);
-        model.addAttribute("predmet", predmet);
-        model.addAttribute("ocjene", ocjene);
-        model.addAttribute("averageOcjena", averageOcjena);
-        
-        return "ocjene-predmeta";
-}
-
-@GetMapping("/user-dashboard/predmeti/{predmetId}/ocjene")
-public String listOcjeneForPredmetForUcenik(@PathVariable Long predmetId, Model model) {
-    // Autentikacija korisnika
-    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    String username = null;
-    if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        username = userDetails.getUsername();
-    } else if (authentication != null) {
-        username = authentication.getName();
+    @GetMapping("/ocjene/{ocjenaId}/edit-ocjena")
+    public String showEditOcjenaForm(@PathVariable Long ocjenaId, Model model){
+        Ocjene ocjene = ocjeneService.getById(ocjenaId);
+        model.addAttribute("ocjena", ocjene);
+        return "edit-ocjena";
     }
 
-    // Dobiti korisnika preko username
-    Korisnik korisnik = korisnikService.findKorisnikByUsername(username);
-    
-    // Dobiti predmet id 
-    Predmet predmet = predmetService.getPredmetById(predmetId);
-    
-    // Dobiti ocjene za predmete
-    List<Ocjene> ocjene = ocjeneService.findOcjeneByKorisnikAndPredmet(korisnik, predmet);
-    // Get the average ocjena for the predmet
-    Long korisnikId = korisnik.getId();
-    Double averageOcjena = ocjeneService.findAverageOcjenaByPredmetIdAndKorisnikId(predmetId, korisnikId);
-    
-    model.addAttribute("korisnik", korisnik);
-    model.addAttribute("predmet", predmet);
-    model.addAttribute("ocjene", ocjene);
-    model.addAttribute("averageOcjena", averageOcjena);
-    
-    return "ocjene-predmeta-ucenika";
-}
-
-    //Ocjene ucenika za razrednika
-    @GetMapping("/razredi/ucenici/predmeti/ocjene")
-    public String listOcjeneForPredmetForRazrednik(@RequestParam("razredId") Long razredId, @RequestParam("korisnikId") Long korisnikId, @RequestParam("predmetId") Long predmetId, Model model) {
-        // Dohvati korisnika i predmet
-        Korisnik korisnik = korisnikService.findKorisnikById(korisnikId);
-        Predmet predmet = predmetService.getPredmetById(predmetId);
-        Razred razred = razredService.findById(razredId);
-        
-        // Dohvati sve ocjene za korisnika i predmet
-        List<Ocjene> ocjene = ocjeneService.findOcjeneByKorisnikAndPredmet(korisnik, predmet);
-        // Get the average ocjena for the predmet
-        Double averageOcjena = ocjeneService.findAverageOcjenaByPredmetIdAndKorisnikId(predmetId, korisnikId);
-        
-        // Pass the grades to the view
-        model.addAttribute("korisnik", korisnik);
-        model.addAttribute("predmet", predmet);
-        model.addAttribute("ocjene", ocjene);
-        model.addAttribute("razred", razred);
-        model.addAttribute("averageOcjena", averageOcjena);
-        
-        return "razrednik-ocjene-ucenika";
-}
-
-       //Ocjene ucenika za nastavnika
-       @GetMapping("/{korisnikId}/predmeti/{predmetId}/ocjene")
-       public String listOcjeneForPredmetForNastavnik(@PathVariable Long korisnikId, @PathVariable Long predmetId, Model model) {
-           
-           // Dohvati korisnika i predmet
-        Korisnik korisnik = korisnikService.findKorisnikById(korisnikId);
-        Predmet predmet = predmetService.getPredmetById(predmetId);
-        
-        // Dohvati sve ocjene za korisnika i predmet
-        List<Ocjene> ocjene = ocjeneService.findOcjeneByKorisnikAndPredmet(korisnik, predmet);
-        // Get the average ocjena for the predmet
-        Double averageOcjena = ocjeneService.findAverageOcjenaByPredmetIdAndKorisnikId(predmetId, korisnikId);
-        
-        // Pass the grades to the view
-        model.addAttribute("korisnik", korisnik);
-        model.addAttribute("predmet", predmet);
-        model.addAttribute("ocjene", ocjene);
-        model.addAttribute("averageOcjena", averageOcjena);
-           
-           return "nastavnik-ocjene-ucenika";
-   }
+    @PostMapping("/edit-ocjena/{ocjenaId}")
+    public String editOcjena(@PathVariable Long ocjenaId, Model model, Ocjene updatedOcjena){
+        Ocjene ocjene = ocjeneService.getById(ocjenaId);
+        ocjene.setOcjena(updatedOcjena.getOcjena());
+        ocjene.setOblast(updatedOcjena.getOblast());
+        ocjene.setDatum(updatedOcjena.getDatum());
+        ocjeneService.saveOcjena(ocjene);
+        return "redirect:/" + ocjene.getKorisnik().getId() + "/predmeti-ocjene";
+    }
 
 }
